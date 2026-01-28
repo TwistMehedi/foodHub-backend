c3f1fb9fbf5ca647cd43bb446e204b1b47f0abad
TwistMehedi <ataul1708@gmail.com>
2026-01-28 17:38:36 +0600

    complate admin route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,3 +1,4 @@
+import { Role } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 import { ErrorHandler } from "../../utils/ErrorHandler";
 import TryCatch from "../../utils/TryCatch";
@@ -30,5 +31,66 @@ export const createCategory = TryCatch(async (req, res, next) => {
     success: true,
     message: "Category created successfully",
     category,
+  });
+});
+
+export const getUsersByAdmin = TryCatch(async (req, res, next) => {
+  const user = req.user;
+
+  if (!user) {
+    return next(new ErrorHandler("Unauthorized", 401));
+  }
+
+  if (user.role !== Role.ADMIN) {
+    return next(new ErrorHandler("You are not authorized", 403));
+  }
+
+  const users = await prisma.user.findMany();
+
+  if (users.length === 0) {
+    return next(new ErrorHandler("No users found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "All users founded",
+    users,
+  });
+});
+
+export const updateUserStatusByAdmin = TryCatch(async (req, res, next) => {
+  const admin = req.user;
+  const userId = req.params.id as string;
+  const { status } = req.body;
+
+  if (!admin || admin.role !== Role.ADMIN) {
+    return next(new ErrorHandler("You are not authorized", 403));
+  }
+
+  if (!userId) {
+    return next(new ErrorHandler("User id is required", 400));
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  if (user.id === admin.id) {
+    return next(new ErrorHandler("Admin cannot change own status", 400));
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { id: userId },
+    data: { status },
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User status updated successfully",
+    user: updatedUser,
   });
 });
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import { authorizeRoles, middleware } from "../../middleware/middleware";
-import { createCategory } from "./user.controller";
+import {
+  createCategory,
+  getUsersByAdmin,
+  updateUserStatusByAdmin,
+} from "./user.controller";
 
 const router = express.Router();
 
 router
   .route("/create-category")
   .post(middleware, authorizeRoles("ADMIN"), createCategory);
+
+router
+  .route("/users")
+  .get(middleware, authorizeRoles("ADMIN"), getUsersByAdmin);
+
+router
+  .route("/users/:id")
+  .patch(middleware, authorizeRoles("ADMIN"), updateUserStatusByAdmin);
 
 export const userRouter = router;
